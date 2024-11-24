33352db6ab46efb71206bd82819a7e28a36ffe3b
Rrosso27 <125314912+Rrosso27@users.noreply.github.com>
2024-11-24 13:06:41 -0500

    Agregar try catch y Documentación   AuthController

--- app/Auth/Http/Controllers/AuthController.php
+++ app/Auth/Http/Controllers/AuthController.php
@@ -13,17 +13,25 @@ class AuthController extends Controller
 {
     public function register(StoreUsersRequest $request)
     {
-        $user = User::create($request->validated());
-        $token = JWTAuth::fromUser($user);
-        return response()->json(['token' => $token], 201);
+        try {
+            $user = User::create($request->validated());
+            $token = JWTAuth::fromUser($user);
+            return response()->json(['token' => $token], 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'User already exists'], 400);
+        }
     }
 
     public function login(Request $request)
     {
-        $credentials = $request->only('email', 'password');
-        if (!$token = JWTAuth::attempt($credentials)) {
-            return response()->json(['error' => 'Invalid credentials'], 401);
+        try {
+            $credentials = $request->only('email', 'password');
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+            return response()->json(['token' => $token]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Could not create token'], 500);
         }
-        return response()->json(['token' => $token]);
     }
 }
